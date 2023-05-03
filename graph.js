5067570b87a9a6b5d1719ba11ca8af654d57ae72
pgil256 <113308245+pgil256@users.noreply.github.com>
2023-05-03 15:44:47 -0400

    Add files via upload

---
+++ graph.js
@@ -0,0 +1,87 @@
+class Node {
+  constructor(value, adjacent = new Set()) {
+    this.value = value;
+    this.adjacent = adjacent;
+  }
+}
+
+class Graph {
+  constructor() {
+    this.nodes = new Set();
+  }
+
+  addVertex(vertex) {
+    this.nodes.add(vertex);
+  }
+
+  addVertices(vertexArray) {
+    for (let vertex of vertexArray) {
+      this.addVertex(vertex);
+    }
+  }
+
+  addEdge(v1, v2) {
+    v1.adjacent.add(v2);
+    v2.adjacent.add(v1);
+  }
+
+  removeEdge(v1, v2) {
+    v1.adjacent.delete(v2);
+    v2.adjacent.delete(v1);
+  }
+
+  removeVertex(vertex) {
+    for (let node of this.nodes) {
+      if (node.adjacent.has(vertex)) {
+        node.adjacent.delete(vertex);
+      }
+    }
+    this.nodes.delete(vertex);
+  }
+
+  depthFirstSearch(start) {
+    const stack = [start];
+    const result = [];
+    const visited = new Set();
+
+    let currentVertex;
+    visited.add(start);
+    
+    while (stack.length) {
+      currentVertex = stack.pop();
+      result.push(currentVertex.value);
+
+      currentVertex.adjacent.forEach(neighbor => {
+        if (!visited.has(neighbor)) {
+          visited.add(neighbor);
+          stack.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
+
+  breadthFirstSearch(start) {
+    const queue = [start];
+    const result = [];
+    const visited = new Set();
+    let currentVertex;
+
+    visited.add(start);
+
+    while (queue.length) {
+      currentVertex = queue.shift();
+      result.push(currentVertex.value);
+
+      currentVertex.adjacent.forEach(neighbor => {
+        if (!visited.has(neighbor)) {
+          visited.add(neighbor);
+          queue.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
+}
+
+module.exports = { Graph, Node };
